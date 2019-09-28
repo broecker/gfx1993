ef18620f182f085d646c46aeda74d9253847c356
mbroecker <mbrckr@gmail.com>
2019-09-28 11:01:22 -0700

    Added panning to orbital camera
    Very GLUT-centric for now. Mode switched depend on tracking
    button down/up.

--- common/Camera.cpp
+++ common/Camera.cpp
@@ -1,16 +1,18 @@
 #include "Camera.h"
 
-#include <glm/gtx/io.hpp>
 #include <glm/gtx/polar_coordinates.hpp>
 #include <glm/gtx/transform.hpp>
-#include <iostream>
-#include <memory>
 
+using glm::vec2;
 using glm::vec3;
 using glm::vec4;
+using glm::cross;
+using glm::dot;
+using glm::normalize;
 
-OrbitCamera::OrbitCamera(const vec3& t, const vec3& u, float r) : projectionMatrix(glm::perspective(glm::radians(90.f), 1.0f, 1.f, 100.f)),
-	target(t), up(u), position(t), radius(r), phi(0.f), theta(0.f)
+
+OrbitCamera::OrbitCamera(const vec3& t, const vec3& u, float r) : projectionMatrix(glm::perspective(90.f, 1.3f, 1.f, 100.f)),
+	target(t), up(u), position(t), radius(r), phi(0.f), theta(0.f), mode(ROTATE)
 {
 }
 
@@ -19,24 +21,53 @@ void OrbitCamera::handleKeyPress(unsigned char key)
 	switch(key) {
 		case 'a':
 		case '-':
-			radius *= 1.4f;
+			//radius *= 1.4f;
+            radius += 0.4f;
 			break;
 		case 'z':
 		case '=':
 		case '+':
-			radius /= 1.4f;
+		    // Don't go closer than the near plane.
+		    radius = glm::max(1.f, radius -= 0.5f);
+		    //radius -= 0.4f;
 			break;
 		default:
 			break;
 	}
 }
 
+
+void OrbitCamera::handleMousePress(int button, int state) {
+    if (button == 1 && state == 0) {
+        mode = PAN;
+    }
+
+    if (button == 1 && state == 1) {
+        mode = ROTATE;
+    }
+}
+
 void OrbitCamera::handleMouseMove(const glm::ivec2& delta)
 {
-	phi += delta.y;
-	theta += delta.x;
+    if (mode == ROTATE) {
+        phi += delta.y;
+        theta += delta.x;
+        phi = glm::clamp(phi, -89.f, 89.f);
+    }
 
-	phi = glm::clamp(phi, -89.f, 89.f);
+    if (mode == PAN) {
+        // Calculate 3D vector from up and right;
+        vec3 forward = normalize(target - position);
+        vec3 right = cross(forward, up);
+        vec3 realUp = cross(right, forward);
+
+        vec2 movementScale = vec2(-0.2f, 0.2f) * vec2(delta);
+
+        vec3 positionDelta = right * movementScale.x + realUp * movementScale.y;
+
+        target += positionDelta;
+        position += positionDelta;
+    }
 }
 
 glm::mat4 OrbitCamera::getViewMatrix()
--- common/Camera.h
+++ common/Camera.h
@@ -9,6 +9,7 @@ public:
 	virtual ~Camera() = default;
 
 	virtual void handleKeyPress(unsigned char key) = 0;
+	virtual void handleMousePress(int button, int state) = 0;
 	virtual void handleMouseMove(const glm::ivec2& delta) = 0;
 
 	virtual glm::mat4 getViewMatrix() = 0;
@@ -23,6 +24,7 @@ public:
 	OrbitCamera(const glm::vec3& target, const glm::vec3& up, float radius);
 
 	void handleKeyPress(unsigned char key) override;
+    void handleMousePress(int button, int state) override;
 	void handleMouseMove(const glm::ivec2& delta) override;
 
 	glm::mat4 getViewMatrix() override;
@@ -34,6 +36,11 @@ private:
 	glm::vec3		target, up, position;
 	float 			radius;
 	float			phi, theta;
+
+	enum MovementMode {
+	    ROTATE,
+	    PAN
+	}               mode;
 
 	void updatePosition();
 };
