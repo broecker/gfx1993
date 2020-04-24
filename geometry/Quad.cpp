e5a7c37ae4a6d0150a73d30759df63d2e80e1372
mbroecker <mbrckr@gmail.com>
2020-04-23 17:22:48 -0700

    Auto-formatted quad class.

--- geometry/Quad.cpp
+++ geometry/Quad.cpp
@@ -2,37 +2,37 @@
 
 namespace geo
 {
-Quad::Quad(const glm::vec4 &color)
-{
-    using render::Vertex;
-    using glm::vec2;
-    using glm::vec4;
+    Quad::Quad(const glm::vec4 &color)
+    {
+        using render::Vertex;
+        using glm::vec2;
+        using glm::vec4;
 
-    const glm::vec3 normal(0, 0, 1);
+        const glm::vec3 normal(0, 0, 1);
 
-    Vertex va = Vertex(vec4(-1, 1, 0, 1), normal, color, vec2(0, 1));
-    Vertex vb = Vertex(vec4(-1, -1, 0, 1), normal, color, vec2(0, 0));
-    Vertex vc = Vertex(vec4(1, -1, 0, 1), normal, color, vec2(1, 0));
-    Vertex vd = Vertex(vec4(1, 1, 0, 1), normal, color, vec2(1, 1));
+        Vertex va = Vertex(vec4(-1, 1, 0, 1), normal, color, vec2(0, 1));
+        Vertex vb = Vertex(vec4(-1, -1, 0, 1), normal, color, vec2(0, 0));
+        Vertex vc = Vertex(vec4(1, -1, 0, 1), normal, color, vec2(1, 0));
+        Vertex vd = Vertex(vec4(1, 1, 0, 1), normal, color, vec2(1, 1));
 
-    vertices.push_back(va);
-    vertices.push_back(vb);
-    vertices.push_back(vc);
-    vertices.push_back(vd);
+        vertices.push_back(va);
+        vertices.push_back(vb);
+        vertices.push_back(vc);
+        vertices.push_back(vd);
 
-    indices.push_back(0);
-    indices.push_back(1);
-    indices.push_back(2);
-    indices.push_back(2);
-    indices.push_back(3);
-    indices.push_back(0);
+        indices.push_back(0);
+        indices.push_back(1);
+        indices.push_back(2);
+        indices.push_back(2);
+        indices.push_back(3);
+        indices.push_back(0);
 
-    // And back faces (although normal is wrong ... )
-    indices.push_back(0);
-    indices.push_back(2);
-    indices.push_back(1);
-    indices.push_back(2);
-    indices.push_back(0);
-    indices.push_back(3);
-}
+        // And back faces (although normal is wrong ... )
+        indices.push_back(0);
+        indices.push_back(2);
+        indices.push_back(1);
+        indices.push_back(2);
+        indices.push_back(0);
+        indices.push_back(3);
+    }
 }
--- geometry/Quad.h
+++ geometry/Quad.h
@@ -8,16 +8,16 @@
 namespace geo
 {
 
-class Quad : public Geometry
-{
-public:
-    // Constructs a quad from 4 vertices. Assumes the following geometry:
-    // a +---+ d
-    //   | \ |
-    // b +---+ c
-    // The size will be [-1..1] along the XY axis and 0 on the z axis. It can be used for screen-space rendering.
-    explicit Quad(const glm::vec4 &color);
-};
+    class Quad : public Geometry
+    {
+    public:
+        // Constructs a quad from 4 vertices. Assumes the following geometry:
+        // a +---+ d
+        //   | \ |
+        // b +---+ c
+        // The size will be [-1..1] along the XY axis and 0 on the z axis. It can be used for screen-space rendering.
+        explicit Quad(const glm::vec4 &color);
+    };
 
 }
 
